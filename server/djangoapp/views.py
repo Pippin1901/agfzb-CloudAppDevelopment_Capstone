fe180c4907d098890baa71b0178a8179dfcd717b
Pippin1901 <95875674+Pippin1901@users.noreply.github.com>
2022-05-13 14:40:41 +0200

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -16,24 +16,16 @@ logger = logging.getLogger(__name__)
 
 # Create your views here.
 
-
-# Create an `about` view to render a static about page
-#week 1 T4
 def about(request):
     context = {}
     if request.method == "GET":
         return render(request, 'djangoapp/about.html', context)
 
-
-# Create a `contact` view to return a static contact page
-#Week 1 T4
 def contact(request):
     context = {}
     if request.method == "GET":
         return render(request, 'djangoapp/contact.html', context)
 
-# Create a `login_request` view to handle sign in request
-#week 2 t5
 def login_request(request):
     context = {}
     url = "https://08663624.us-south.apigw.appdomain.cloud/api/dealership"
@@ -57,25 +49,19 @@ def login_request(request):
     else:
         return render(request, 'djangoapp/index.html', context)
 
-
-# Create a `logout_request` view to handle sign out request
-# Week 2 T5
 def logout_request(request):
     context = {}
     url = "https://08663624.us-south.apigw.appdomain.cloud/api/dealership"
     dealerships = get_dealers_from_cf(url)
-    # Concat all dealer's short name
+
     context["dealership_list"]=dealerships
-    # Get the user object based on session id in request
+    
     print("Log out the user `{}`".format(request.user.username))
-    # Logout user in the request
+
     logout(request)
-    # Redirect user back to course list view
+
     return render(request, 'djangoapp/index.html', context)
 
-
-# Create a `registration_request` view to handle sign up request
-#week 2 T7
 def registration_request(request):
     context = {}
     # If it is a GET request, just render the registration page
@@ -108,7 +94,6 @@ def registration_request(request):
             context["message"]="Account could not be created try again."
             return render(request, 'djangoapp/registration.html', context)
 
-# Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
     if request.method == "GET":
         context={}
@@ -121,9 +106,6 @@ def get_dealerships(request):
         # Return a list of dealer short name
         return render(request, 'djangoapp/index.html', context)
 
-
-# Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
 def get_dealer_details(request, dealer_id):
     context={}
     url = "https://08663624.us-south.apigw.appdomain.cloud/api/review"
@@ -134,7 +116,7 @@ def get_dealer_details(request, dealer_id):
     context["dealer_id"]=dealer_id
     context["reviews"]=dealer_details
     return render(request, 'djangoapp/dealer_details.html', context)
-# Create a `add_review` view to submit a review
+
 def add_review(request, dealer_id):
     context = {}
     # If it is a GET request, just render the add_review page
